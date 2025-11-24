b85528549a688313c6cf746c9f6b449c2c323372
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-24 23:33:26 +0530

    "function_ex"

---
+++ function_14.js
@@ -0,0 +1,12 @@
+// Write a function to print the multiplication table of a number.
+
+function Table(num) {
+    for (let i = 1; i <= 10; i++) {
+        
+        console.log(num+" X "+i+" = "+ i*num);
+        
+        
+    }
+}
+
+Table(12);
