2d2e26fb4cdbd9c84a89fa5f90a079f34cbc192d
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-24 22:58:39 +0530

    function_ex

---
+++ function_11.js
@@ -0,0 +1,17 @@
+// Write a function to calculate factorial of a number.
+
+function Factorial(num) {
+
+    let fact = 1;
+
+    for (let i = 1; i <= num; i++) 
+    {
+     fact = fact * i;
+
+    }
+
+    return fact;
+}
+
+console.log(Factorial(4));
+
---
+++ function_9.js
@@ -0,0 +1,13 @@
+// Write a function to check if a string is empty.
+
+function EmptyString(str="") {
+    if (str.trim() === "") {
+        console.log("String is empty");
+        
+    }else{
+        console.log("string is not empty");
+        
+    }
+}
+
+EmptyString("Arun");
