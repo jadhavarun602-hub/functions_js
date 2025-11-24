0b3d101fdea1e0c2b79a73ea5d55b97516f78e49
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-24 23:24:16 +0530

    function_13

---
+++ function_12.js
@@ -0,0 +1,11 @@
+// Write a function to convert Celsius to Fahrenheit.
+
+function CelsiusToFahrenheit(C){
+
+    let F = C * 1.8 + 32;
+
+    console.log(F+"F");
+    
+}
+
+CelsiusToFahrenheit(5);
---
+++ function_13.js
@@ -0,0 +1,11 @@
+// Write a function to convert Fahrenheit to Celsius.
+
+function FahrenheitToCelsius(F) {
+    
+    let C = (F - 32) * (5/9);
+
+    console.log(C+"C");
+    
+}
+
+FahrenheitToCelsius(21);
